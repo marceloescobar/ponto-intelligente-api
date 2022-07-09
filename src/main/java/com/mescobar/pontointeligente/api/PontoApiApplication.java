39edfa823783b05e4204bbd0bf65123afe70bc7a
marcelo escobar <escobar.marcelo@gmail.com>
2022-07-09 14:29:20 -0300

    commit inicial do projeto

---
+++ src/main/java/com/mescobar/pontointeligente/api/PontoApiApplication.java
@@ -0,0 +1,13 @@
+package com.mescobar.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoApiApplication.class, args);
+	}
+
+}
